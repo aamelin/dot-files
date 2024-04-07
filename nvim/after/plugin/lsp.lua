be9cfbcf76e9424cd95343a49443042ab32adaff
Andriy Amelin <andriy.m.amelin@gmail.com>
2024-04-08 00:36:18 +0200

    nvim mason-lspconfig for JSON, Lua and Python

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -20,7 +20,7 @@ end)
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
-        'rust_analyzer',
+        'jsonls', 'lua_ls', 'pylsp', 'rust_analyzer'
     },
     handlers = {
         lsp_zero.default_setup,
